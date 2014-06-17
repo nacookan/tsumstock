333b32066443085289ff2badf7c98c5a9e1059ad
nacookan <nacookan@gmail.com>
2014-06-17 17:50:41 +0900

    First commit!

---
+++ config.rb
@@ -0,0 +1,7 @@
+$twitter = {
+  :consumer_key => 'XXXXXXXX',
+  :consumer_secret => 'XXXXXXXX',
+  :access_token => 'XXXXXXXX',
+  :access_token_secret => 'XXXXXXXX',
+}
+$link = 'http://www.disneystore.co.jp/shop/ProductDetail.aspx?sku=[[SKU]]'
